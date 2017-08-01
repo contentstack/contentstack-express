cd92cb7e4f863a478235901b4581ce98ba1ab462
Aamod Pisat <aamod.pisat@raweng.com>
2017-08-01 17:40:46 +0530

    asset versioning in cs-express@1.0.7 added asset delete operation added examples

---
+++ examples/demo/config/all.js
@@ -0,0 +1,14 @@
+module.exports = exports = {
+    port: 5000,
+    theme: "basic",
+    languages: [
+        {
+            "code": "en-us",
+            "relative_url_prefix": "/"
+        }
+    ],
+    contentstack: {
+        "api_key": "api_key",
+        "access_token": "asset_token"
+    }
+};
