f2a697c63f0ba7b21c6eeaee2a7ed4712b0318a2
Aamod Pisat <aamod.pisat@raweng.com>
2017-10-10 22:37:59 +0530

    added regex for asset filename

--- examples/demo/config/all.js
+++ examples/demo/config/all.js
@@ -2,13 +2,17 @@ module.exports = exports = {
     port: 5000,
     theme: "basic",
     languages: [
-        {
-            "code": "en-us",
-            "relative_url_prefix": "/"
-        }
+    {
+        "code": "en-us",
+        "relative_url_prefix": "/"
+    }
     ],
     contentstack: {
-        "api_key": "api_key",
-        "access_token": "access_token"
+        // host: "http://172.16.0.77:8000",
+        // socket: "http://172.16.0.77:8001/",
+        host: "https://pt-new-api.contentstack.io",
+        socket: "https://pt-realtime.contentstack.io/",
+        api_key: "blt159b922b70c6447e",
+        access_token: "blt26849b2fad36e638"
     }
 };
